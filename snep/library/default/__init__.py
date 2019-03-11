949a81a3d60dc2353110a1e6145909e9391f106d
Paola Suárez <pao.2702@gmail.com>
2019-03-11 13:10:11 +0100

    rm snep

--- snep/library/default/__init__.py
+++
@@ -1,8 +0,0 @@
-from snep.configuration import config
-
-if config['type'] == 'brian':
-    from .default_brian import *
-elif config['type'] == 'none':
-    from .default_empty import *
-else:
-    raise Exception('Unknown simulation type. Check your configuration.')
