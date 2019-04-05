628f8dc6a1f19e3399ef3211df9118667d8b7d1f
Paola Suárez <pao.2702@gmail.com>
2019-04-05 08:49:46 +0200

    includes spikes and param dict to test burst analysis

--- tests.py
+++ tests.py
@@ -1,5 +1,8 @@
 import unittest
+import numpy as np
 from helper_funcs import validate_file, handle_brian_units
+from burst_analysis import spks2neurometric
+from brian2.units import second, Hz, pA, ms
 
 
 class HelperFuncsTest(unittest.TestCase):
@@ -18,6 +21,52 @@ class HelperFuncsTest(unittest.TestCase):
         self.assertEqual(handle_brian_units('5'), 5)
         self.assertEqual(handle_brian_units('False'), None)
         self.assertEqual(handle_brian_units('kk'), None)
+
+    def test_burst_analysis_correct_isis(self):
+        spks2test = np.loadtxt('spksSE.csv')
+        spks2test = spks2test[np.newaxis, :]
+        task_info = {
+            'dec': {
+                'N_E': 1600,
+                'N_I': 400,
+                'sub': 0.15},
+
+            'sen': {
+                'N_E': 1600,
+                'N_I': 400,
+                'N_X': 1000,
+                'sub': 0.5},
+
+            'sim': {
+                'sim_dt': 0.1*ms,
+                'stim_dt': 1*ms,
+                'runtime': 3*second,
+                'settle_time': 0*second,
+                'stim_on': 0.5*second,
+                'stim_off': 2.5*second,
+                'replicate_stim': False,
+                'num_method': 'euler',
+                'seed_con': 1284,
+                'smooth_win': 100*ms,
+                'valid_burst': 16e-3,
+                '2c_model': True,
+                'plt_fig1': False,
+                'burst_analysis': True,
+                'plasticity': False},
+
+            'plastic': {
+                # 'targetB': Parameter(2, 'Hz'),
+                'tauB': 50000*ms,
+                'tau_update': 10*ms,
+                'eta0': 5*pA,
+                'min_burst_stop': 0.1,
+                'dec_winner_rate': 35*Hz},
+
+            'c': 0,
+            'bfb': 0,
+            'targetB': 2*Hz}
+
+        self.assertEqual(spks2neurometric(task_info, spks2test, raster=True), None)
 
     def test_brian2param(self):
         pass
